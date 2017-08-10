4acf89f1e40337686f62b1cea5a7fb91f4d3bd07
ShashyCuber <shashwat.sparsh@gmail.com>
2017-08-10 09:48:11 -0700

    Experimentaion with \ outputs
    added: \t \n \" and \\

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,19 @@
 
-public class HelloWorld {
+public class HelloWorld 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
+		System.out.print("Hi");
+		System.out.println("Hi");
+		
+		System.out.println("What does\tprint out?");
+		System.out.println("Whatdoes\nprint out?");
+		System.out.println("What does \" print out?");
+		System.out.println("How many \\ are printed?");
+	
+	
 	}
 
 }
